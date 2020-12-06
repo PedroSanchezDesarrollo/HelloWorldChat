f7073c19a81ddfc8d2ef65e7d77adf4ab9b706b1
Pedro J. Sanchez Cosme <pedrojosesanchezcosme@gmail.com>
2020-12-06 18:02:18 +0100

    Limpiar app.componet, importar js y css, crear componente para el chat y configurar proyecto para los selectores de los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatViewComponent } from './chat-view/chat-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatViewComponent
   ],
   imports: [
     BrowserModule,

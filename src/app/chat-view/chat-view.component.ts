1994754a3c4fb88bc8fbba4227657efe624ca27c
Pedro J. Sanchez Cosme <pedrojosesanchezcosme@gmail.com>
2020-12-08 00:16:40 +0100

    Configuracion de la interfaz del chat, producto minimo viable

--- src/app/chat-view/chat-view.component.ts
+++ src/app/chat-view/chat-view.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatViewComponent implements OnInit {
 
+  listaMensajes = [
+      {
+        "idUsuario": 0,
+        "mensaje": "Hola soy Pedro, que tal estais?",
+        "datetime": "2020-12-07 22:00:00"
+      },
+      {
+        "idUsuario": 1,
+        "mensaje": "Hola soy Pablo, que tal estais?",
+        "datetime": "2020-12-07 22:01:00"
+      },
+      {
+        "idUsuario": 0,
+        "mensaje": "Encantado",
+        "datetime": "2020-12-07 22:02:00"
+      },
+      {
+        "idUsuario": 0,
+        "mensaje": ":)",
+        "datetime": "2020-12-07 22:02:00"
+      },
+      {
+        "idUsuario": 2,
+        "mensaje": "Holiiii",
+        "datetime": "2020-12-07 22:04:00"
+      }
+    ];
+
   constructor() { }
 
   ngOnInit(): void {
